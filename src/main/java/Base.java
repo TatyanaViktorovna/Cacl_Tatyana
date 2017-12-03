4fdcb673d38cb4e8ca2ae29a6239ada2b767b79d
Gryaznova <gryaznova89@gmail.com>
2017-12-04 01:03:49 +0400

    version 3

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
  *         Created by Таня on 03.12.2017.
  */
 public class Base {
-    public static void main(String[] args) {
+    public static final void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Chislo 1: ");
         double i = scn.nextDouble();
         System.out.println("Chislo 2: ");
         double n = scn.nextDouble();
         double s = i + n;
-        System.out.println("Summa: "+ s);
+        System.out.printf("Summa: %.4f",s); // сумма с округлением до 4 знака после запятой
+        //System.out.println("Summa: "+ s); // сумма без округления
         //     System.err.println("Error(");
         scn.close();
     }
