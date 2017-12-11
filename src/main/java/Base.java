d31071f71507bf8fbc160e7c40c433d762ca1b32
TatyanaViktorovna <www.tanechka_@mail.tu>
2017-12-11 18:58:32 +0300

    Version 4

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -10,18 +10,18 @@ public class Base {
         System.out.println("Chislo 1: ");
         double i = scn.nextDouble();
         System.out.println("Operation 1 - '+', 2 - '-', 3 - '/', 4 - '*'");
-        int d = scn.nextInt();
+        char d = scn.next().charAt(0);
 
         System.out.println("Chislo 2: ");
         double n = scn.nextDouble();
         switch (d){
-            case 1: System.out.printf("Summa: %.4f",i+n);
+            case '+': System.out.printf("Summa: %.4f",i+n);
                 break;
-            case 2: System.out.printf("Raznica: %.4f",i-n);
+            case '-': System.out.printf("Raznica: %.4f",i-n);
                 break;
-            case 3: System.out.printf("Chastnoe: %.4f",i/n);
+            case '/': System.out.printf("Chastnoe: %.4f",i/n);
                 break;
-            case 4: System.out.printf("Proizvedenie: %.4f",i*n);
+            case '*': System.out.printf("Proizvedenie: %.4f",i*n);
                 break;
         }
 //        //System.out.println("Summa: "+ s); // сумма без округления
